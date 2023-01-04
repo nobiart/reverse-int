6aa3fa2d70cb829bab2ba0f97ba4d7fc3c821adc
nobiart <dmitri.besleaga@gmail.com>
2023-01-04 22:34:51 +0200

    feat: create reverse-int function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+    const stringFromNumber = n.toString();
+    let result = '';
+
+    for (let i = stringFromNumber.length - 1; i >= 0 ; i--) {
+        if (!isNaN(+stringFromNumber[i])) {
+            result += stringFromNumber[i];
+        }
+    }
+
+    return Number(result);
 }
